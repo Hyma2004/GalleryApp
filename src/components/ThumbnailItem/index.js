610d4fcfe524c358df4d122d98cfa25f8ca0d2f3
Hyma2004 <hymatamarana2000@gmail.com>
2024-04-30 14:36:41 +0000

    Gallery app

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {imageDetails, isActive, changeImage} = props
+  const {thumbnailUrl, thumbnailAltText, id} = imageDetails
+  const activeClassName = isActive ? 'active-container' : 'image-container'
+  const onChangeImage = () => {
+    changeImage(id)
+  }
+  return (
+    <li className={activeClassName}>
+      <button type="button" className="btn">
+        <img
+          src={thumbnailUrl}
+          alt={thumbnailAltText}
+          className="thumbnail"
+          onClick={onChangeImage}
+        />
+      </button>
+    </li>
+  )
+}
+
+export default ThumbnailItem
